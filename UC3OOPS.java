b2b88734555ad818b73326a3cbbe3787fb79ff10
an4761 <an4761@srmist.edu.in>
2026-02-17 21:22:37 +0530

    UC3 Refactored banner using String.join

---
+++ UC3OOPS.java
@@ -0,0 +1,25 @@
+public class UC3OOPS {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " *****", " *****", " *****", " *****"));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*    *", "*"));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*****", " *****"));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*", "      *"));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*", "      *"));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*", "*"));
+
+        System.out.println(String.join("*",
+                " *****", " *****", "*", "    *****"));
+    }
+}
